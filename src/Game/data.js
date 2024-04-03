1a9a859aab09e85fac23c4db5b4338cfa5ee9c49
Hana Alebachew Mengist <hanialex1116@gmail.com>
2024-04-03 09:33:31 +0530

    memory card game

---
+++ src/Game/data.js
@@ -0,0 +1,75 @@
+import { betroot, broccoli, cabage, tomato, lettuce, potato } from "./imports";
+export const Data = [
+  {
+    id: 1,
+    name: "react",
+    img: betroot,
+    matched: false,
+  },
+  {
+    id: 2,
+    name: "java",
+    img: broccoli,
+    matched: false,
+  },
+  {
+    id: 3,
+    name: "css",
+    img: cabage,
+    matched: false,
+  },
+  {
+    id: 4,
+    name: "node",
+    img: tomato,
+    matched: false,
+  },
+  {
+    id: 5,
+    name: "html",
+    img: lettuce,
+    matched: false,
+  },
+  {
+    id: 6,
+    name: "js",
+    img: potato,
+    matched: false,
+  },
+  {
+    id: 7,
+    name: "react",
+    img: betroot,
+    matched: false,
+  },
+  {
+    id: 8,
+    name: "java",
+    img: broccoli,
+    matched: false,
+  },
+  {
+    id: 9,
+    name: "css",
+    img: cabage,
+    matched: false,
+  },
+  {
+    id: 10,
+    name: "node",
+    img: tomato,
+    matched: false,
+  },
+  {
+    id: 11,
+    name: "html",
+    img: lettuce,
+    matched: false,
+  },
+  {
+    id: 12,
+    name: "js",
+    img: potato,
+    matched: false,
+  },
+];
---
+++ src/Game/imports.js
@@ -0,0 +1,8 @@
+import betroot from "../assets/beetroot.png";
+import broccoli from "../assets/broccoli.png";
+import cabage from "../assets/cabage.png";
+import potato from "../assets/potato.png";
+import lettuce from "../assets/lettuce.png";
+import tomato from "../assets/tomato.png";
+
+export { betroot, broccoli, cabage, potato, lettuce, tomato };
